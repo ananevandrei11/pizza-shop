c2910b1ef00f7492526561546dddf63b0f567ada
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2024-08-10 18:17:13 +0200

    feat add shadcn lucide first components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,31 @@
+export { Button, buttonVariants } from "./button"
+export { Checkbox } from "./checkbox"
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from "./dialog"
+export {
+  Drawer,
+  DrawerPortal,
+  DrawerOverlay,
+  DrawerTrigger,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerDescription,
+} from "./drawer"
+export { Input } from "./input"
+export { Popover, PopoverTrigger, PopoverContent } from "./popover"
+export { RadioGroup, RadioGroupItem } from "./radio-group"
+export { Select } from "./select"
+export { Slider } from "./slider"
