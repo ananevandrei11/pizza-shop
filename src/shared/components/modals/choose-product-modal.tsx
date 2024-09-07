85a8ebc81656e34588def63a853dd532f947e5c5
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2024-09-07 22:04:43 +0200

    feat: create page product

--- src/shared/components/modals/choose-product-modal.tsx
+++ src/shared/components/modals/choose-product-modal.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import toast from 'react-hot-toast';
 
 import { cn } from '@/shared/lib/utils';
 import { ProductWithRelations } from '@/types/prisma';
 import { Dialog, DialogContent, DialogTitle } from '../ui';
-import { ChoosePIzzaForm } from './choose-pizza-form';
-import { ChooseProductForm } from './choose-product-form';
-import { useCartStore } from '@/shared/store';
+import { ProductForm } from './product-form';
 
 interface Props {
   product: ProductWithRelations;
@@ -17,60 +14,20 @@ interface Props {
 
 export const ChooseProductModal = ({ product, className }: Props) => {
   const router = useRouter();
-  const firstItem = product.items?.[0];
-  const isPizzaForm = Boolean(firstItem.pizzaType);
 
-  const { loading, addCartItem } = useCartStore(state => ({
-    addCartItem: state.addCartItem,
-    loading: state.loading,
-  }));
-
-  const onSubmit = async (productItemId?: number, ingredients?: number[]) => {
-    try {
-      const itemId = productItemId || firstItem.id;
-      await addCartItem({
-        productItemId: itemId,
-        ingredients: ingredients || undefined,
-      });
-      router.back();
-      toast.success(`${product.name} is added`);
-    } catch (err) {
-      const message =
-        err instanceof Error
-          ? err.message
-          : `Something went wrong while adding ${product.name.toLowerCase()}`;
-      toast.error(message);
-    }
+  const handleSubmit = () => {
+    router.back();
   };
 
   return (
-    <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
+    <Dialog open={Boolean(product)} onOpenChange={handleSubmit}>
       <DialogContent
         className={cn('p-0 max-w-[1060px] max-h-[90dvh] bg-white overflow-hidden', className)}
         aria-describedby="Choose pizza or product"
       >
         <div className="w-full h-full max-h-[inherit] overflow-auto scrollbar">
           <DialogTitle className="hidden" title="Choose pizza or product" />
-          {isPizzaForm ? (
-            <ChoosePIzzaForm
-              imageUrl={product.imageUrl}
-              name={product.name}
-              ingredients={product.ingredients}
-              items={product.items}
-              onSubmit={onSubmit}
-              loading={loading}
-              className="w-full h-full"
-            />
-          ) : (
-            <ChooseProductForm
-              imageUrl={product.imageUrl}
-              name={product.name}
-              price={firstItem.price}
-              onSubmit={() => onSubmit()}
-              loading={loading}
-              className="w-full h-full"
-            />
-          )}
+          <ProductForm product={product} variant="modal" onSubmit={handleSubmit} />
         </div>
       </DialogContent>
     </Dialog>
--- src/shared/components/modals/index.ts
+++ src/shared/components/modals/index.ts
@@ -1,3 +1,4 @@
 export { ChooseProductModal } from './choose-product-modal';
 export { ChoosePIzzaForm } from './choose-pizza-form';
 export { ChooseProductForm } from './choose-product-form';
+export { ProductForm } from './product-form';
